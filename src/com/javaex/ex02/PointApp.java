e9f399b2bc7b56e9b49ca5003b33cab4ccf0bbda
seed-yoo <lukdown10@naver.com>
2024-01-11 12:09:11 +0900

    Object 클래스

--- src/com/javaex/ex02/PointApp.java
+++ src/com/javaex/ex02/PointApp.java
@@ -7,6 +7,7 @@ public class PointApp {
 		Point p00 = new Point(3, 5);
 		Point p01 = new Point(3, 5);
 		Point p02 = new Point(13, 15);
+		Point p03 = p02;
 		
 		// 클래스 정보보기
 		System.out.println(p00.getClass());
@@ -19,6 +20,23 @@ public class PointApp {
 		System.out.println(p00.toString());
 		System.out.println(p01);
 		System.out.println(p02.toString());
+		
+		System.out.println("=================================");
+		
+		// hashCode()
+		System.out.println(p00.hashCode());
+		System.out.println(p01.hashCode());
+		System.out.println(p02.hashCode());
+		
+		System.out.println("=================================");
+		
+		// equals()
+		System.out.println(p00.equals(p01));	// p00 부모의 메소드 사용(로직은 같다)
+		System.out.println(p01.equals(p00));	// p01 부모의 메소드 사용(로직은 같다)
+		
+		System.out.println(p02.equals(p03));	// 같다
+		
+		
 		
 	}
 }
